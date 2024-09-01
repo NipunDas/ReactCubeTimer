93b0ee184e5cc2017b45b75bafb211099168d747
Cody Tran <codysandiego@gmail.com>
2024-08-31 20:01:58 -0700

    timer activates via spacebar

--- cube-timer/src/Timer.tsx
+++ cube-timer/src/Timer.tsx
@@ -1,47 +1,79 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useEffect } from 'react';
+import { useState, useRef } from 'react';
 import './css/Timer.css'
 
-interface TimeEntry {
-	time: string;
-	ao5: string;
-	ao12: string;
-}
+// interface TimeEntry {
+// 	time: number;
+// 	ao5: number;
+// 	ao12: number;
+// }
 
 function Timer() {
-    const [ startDate, setStartDate ] = useState<Date>(new Date());
-    const [ timing, setTiming ] = useState<boolean>();
-    const [ time , setTime ] = useState<string>('0');
-    const [ timerInterval, setTimerInterval ] = useState<NodeJS.Timeout>();
+    const [ startTime, setStartTime ] = useState<number>(0);
+    const [ now, setNow ] = useState<number>(0);
+    const [ timing, setTiming ] = useState<boolean>(false); // used to prevent back-to-back triggering of timer
+    const [ keyDown, setKeyDown ] = useState<boolean>(false); // To avoid key press repeats when holding down space, event.repeat is buggy
+    const [ timeColor, setTimeColor ] = useState<string>("black");
 
-    function updateCurrentTime() {
-        setTime(((new Date().getTime() - startDate.getTime()) / 1000).toFixed(2));
+    const timerInterval = useRef<NodeJS.Timeout>();
+
+    let time: number = 0;
+    if (startTime != null && now != null) {
+        time = (now - startTime) / 1000;
     }
 
     function startTimer() {
-        if (!timing) {
-            setStartDate(new Date());
-            setTimerInterval(setInterval(() => {
-                setTime((((new Date()).getTime() - startDate.getTime()) / 1000).toFixed(1));
-            }, 10));
-            setTiming(true);
-        }
+        setStartTime(Date.now());
+        setNow(Date.now());
+
+        // clearInterval(timerInterval.current);
+        timerInterval.current = setInterval(() => {
+            setNow(Date.now());
+        }, 10);
     }
     
     function endTimer() {
-        if (timing) {
-            clearInterval(timerInterval);
-            updateCurrentTime();
-            setTiming(false);
+        clearInterval(timerInterval.current);
+    }
+    
+    function handleKeyDown(event: KeyboardEvent) {
+        if (keyDown) {
+            return;
+        }
 
+        setKeyDown(true);
+        if (timing) {
+            endTimer();
+        } else if (event.key === ' ') {
+            setTimeColor('green');
         }
     }
+    
+    function handleKeyUp(event: KeyboardEvent) {
+
+        if (event.key === ' ' && !timing) {
+            setTiming(true);
+            startTimer();
+            setTimeColor('black');
+        } else if (timing) {
+            setTiming(false);
+        }
+        setKeyDown(false);
+    }
+
+    useEffect(() => {
+        // I don't know if this is good in React but hopefully it is
+        document.addEventListener('keydown', handleKeyDown);
+        document.addEventListener('keyup', handleKeyUp);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            document.removeEventListener('keyup', handleKeyUp);
+        }
+    });
 
     return (
         <div>
-            <div className="timer">{time}</div>
-            <button onClick={startTimer}>start</button>
-            <button onClick={endTimer}>end</button>
+            <div className="timer" style={{color: timeColor}}>{time.toFixed(1)}</div>
         </div>
     );
 }
